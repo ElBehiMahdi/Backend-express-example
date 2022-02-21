194419d5dbc774f1b76668ecbb27ee207808101b
MahdiBehi <78915698+MahdiBehiEsprit@users.noreply.github.com>
2022-02-21 13:49:53 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  /* connect locally 
+  .connect(process.env.DATABASE_LOCAL,{//.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB Connection successful'));
+  */
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB Connection successful'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
